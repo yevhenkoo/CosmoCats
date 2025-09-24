de723069f501a314522cb7a6dcaf581d15a87f9e
yevhenkoo <evg.onk16@gmail.com>
2025-09-24 15:08:41 +0300

    Start comit

---
+++ src/main/java/org/example/cosmocats/CosmoCatsApplication.java
@@ -0,0 +1,13 @@
+package org.example.cosmocats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmoCatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CosmoCatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cosmocats/CosmoCatsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cosmocats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmoCatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
